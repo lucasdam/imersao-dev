bea6fb4571d066b511ab4394fcc92d2e39bc1755
Lucas Damasceno <54273070+lucasdam@users.noreply.github.com>
2022-03-09 16:32:47 -0300

    Adicionado botão de reiniciar no desafio Mentalista

--- mentalista/main.js
+++ mentalista/main.js
@@ -1,15 +1,20 @@
 let secretNumber = parseInt(Math.random() * 11)
 let triesLeft = 3
 
+let guess = document.getElementById('input-value').focus()
+let result = document.querySelector('#result-value')
+let tries = document.querySelector('#tries-value')
+let guessButton = document.getElementById('guess-button')
+let restartButton = document.getElementById('restart-button')
+
 console.log('Secret Number: ' + secretNumber)
 
 function toGuess() {
 
-    let guess = parseInt(document.querySelector('#input-value').value)
-    let result = document.querySelector('#result-value')
-    let tries = document.querySelector('#tries-value')
+    guess = parseInt(document.getElementById('input-value').value)
 
-    tries.innerHTML = `Número de tentativas: ${--triesLeft}`
+    restartButton.classList.add('disable-button')
+    tries.innerHTML = `Tentativas restantes: ${--triesLeft}`
 
     let isCorrect = guess === secretNumber ? true : false
     let hasTries = triesLeft > 0 ? true : false
@@ -19,15 +24,24 @@ function toGuess() {
 
         if (isCorrect) {
 
-            result.innerHTML = `Você acertou! Era ${secretNumber}`
+            result.innerHTML = `Você acertou! Era ${secretNumber} mesmo.`
+
+            guessButton.classList.add('disable-button')
+            restartButton.classList.remove('disable-button')
     
         } else {
     
             result.innerHTML = `Não é ${guess}... `
-            result.innerHTML += guess > secretNumber ? 'Tente um número menor' : 'Tente um número maior'
+            result.innerHTML += guess > secretNumber ? 'Tente um número menor.' : 'Tente um número maior.'
+
+            document.getElementById('input-value').value = ''
+            guess = document.getElementById('input-value').focus()
     
             if (!hasTries) {
-                result.innerHTML = `Suas tentativas acabaram... O número era ${secretNumber}`
+                result.innerHTML = `Suas tentativas acabaram... O número era ${secretNumber}.`
+
+                guessButton.classList.add('disable-button')
+                restartButton.classList.remove('disable-button')
             }
     
         }
@@ -35,7 +49,32 @@ function toGuess() {
     } else {
 
         result.innerHTML = 'Opa! Tenta um número de 0 a 10.'
+        tries.innerHTML = `Tentativas restantes: ${++triesLeft}`
+
+        document.getElementById('input-value').value = ''
+        guess = document.getElementById('input-value').focus()
 
     }
 
 }
+
+
+function restart() {
+
+    secretNumber = parseInt(Math.random() * 11)
+    triesLeft = 3
+    
+    tries.innerHTML = `Tentativas restantes: 3`
+    result.innerHTML = ''
+
+    document.getElementById('input-value').value = ''
+    document.getElementById('input-value').focus()
+
+    restartButton.classList.add('disable-button')
+    guessButton.classList.remove('disable-button')
+
+    console.log('Secret Number: ' + secretNumber)
+
+}
+
+/* OBS: Muito código repetido que precisa ser melhorado... */
