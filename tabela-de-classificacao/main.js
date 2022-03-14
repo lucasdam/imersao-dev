6d5fcde7fc0aa07fe8435bb08c398a9ca926ddeb
Lucas Damasceno <54273070+lucasdam@users.noreply.github.com>
2022-03-14 18:01:56 -0300

    Adicionado localStorage e função para remover jogador

--- tabela-de-classificacao/main.js
+++ tabela-de-classificacao/main.js
@@ -8,13 +8,21 @@ const Player = class {
     }
 }
 
-let players = []
+const localStoragePlayers = JSON.parse(localStorage.getItem('tabela'))
+let players = localStorage.getItem('tabela') !== null ? localStoragePlayers : []
+
+const updateLocalStorage = () => {
+    localStorage.setItem('tabela', JSON.stringify(players))
+}
+
+showPlayers()
 
 function addPlayer() {
     const newPlayer = document.getElementById('input-player').value
     const player = new Player(newPlayer, 0, 0, 0, 0)
     document.getElementById('input-player').value = ''
     players.push(player)
+    updateLocalStorage()
     player.scores = calculateScores(player)
     showPlayers()
 }
@@ -38,6 +46,7 @@ function showPlayers() {
                 <td><button onClick="addWin(${i})">Vitória</button></td>
                 <td><button onClick="addTie(${i})">Empate</button></td>
                 <td><button onClick="addLoss(${i})">Derrota</button></td>
+                <td><button onClick="deletePlayer(${i})">X</button></td>
             </tr>
         `
     }
@@ -49,6 +58,7 @@ function addWin(i) {
     const player = players[i]
     player.wins++
     players[i].scores = calculateScores(player)
+    updateLocalStorage()
     showPlayers(players)
 }
 
@@ -56,18 +66,40 @@ function addTie(i) {
     const player = players[i]
     player.ties++
     players[i].scores = calculateScores(player)
+    updateLocalStorage()
     showPlayers(players)
 }
 
 function addLoss(i) {
     const player = players[i]
     player.losses++
+    updateLocalStorage()
     showPlayers(players)
+}
+
+function resetScore() {
+    if (confirm('Deseja apagar a pontuação da tabela?')) {
+        players.forEach(player => {
+            player.wins = 0
+            player.ties = 0
+            player.losses = 0
+            player.scores = 0
+        })
+        updateLocalStorage()
+        showPlayers()
+    }
+}
+
+function deletePlayer(i) {
+    players.splice(i, 1)
+    updateLocalStorage()
+    showPlayers()
 }
 
 /* DESAFIOS
 1. [ ] Quando houver um empate, ajustar como empate para todos os jogadores
 2. [ ] Adicionar a imagem de cada jogador
-3. [ ] Criar um botão para zerar todos os pontos
+3. [X] Criar um botão para zerar todos os pontos
 4. [X] Criar um botão e inputs para adicionar novos jogadores 
-5. [ ] Armazenar os dados no localStorage */
+5. [X] Armazenar os dados no localStorage 
+6. [X] Remover jogador da tabela */
